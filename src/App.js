7219b691bfc38816397870ba58cf140c66c8e3a0
Behrooz Behamooz <behrooz.b.ir@gmail.com>
2021-04-16 18:47:32 -0700

    trying to push to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router";
+import {Link, useLocation} from "react-router-dom";
+
+import Header from "./common/Header";
+import CardList from "./home/CardList";
+import User from "./user/User";
+import NotFound from "./common/NotFound";
 
 function App() {
+  /*
+    TODO: There is no need to add a <Router >, it is in index.js.
+
+    The <CardList /> component should be shown when the user is on the index path.
+
+    The <User /> component should be shown when the user is on the following path:
+    /users/:userId
+
+    Display <NotFound /> when appropriate
+  */
+  console.log("render App");
+  const location=useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <Switch>
+          <Route path="/" exact={true}>
+            <CardList />
+          </Route>
+          <Route path={"/users/:userId"}>
+            <User />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+      </Switch>
+      
+    </Fragment>
   );
 }
 
